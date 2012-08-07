9c85d41befbdc6a5e060be57251a3d55b93bd350
Flavio Martins <xhaker@gmail.com>
2012-08-07 12:57:54 +0100

    Updates

--- src/main/java/pt/unl/fct/di/Main.java
+++ src/main/java/pt/unl/fct/di/Main.java
@@ -6,17 +6,16 @@ import java.util.Scanner;
 import org.apache.lucene.util.Version;
 
 import at.ac.univie.mminf.luceneSKOS.search.SKOSAutocompleter;
-import at.ac.univie.mminf.luceneSKOS.search.spell.SKOSSuggester;
 
 public class Main {
   
   public static void main(String[] args) throws IOException {
     String skosFile = "../../skos/eurovoc_skos.rdf.zip";
     
-    SKOSSuggester skosSuggester = new SKOSSuggester(Version.LUCENE_40,
-        skosFile, "en");
-    SKOSAutocompleter skosAutocompleter = new SKOSAutocompleter(Version.LUCENE_40,
-        skosFile, "en");
+    // SKOSSuggester skosSuggester = new SKOSSuggester(Version.LUCENE_40,
+    // skosFile, "en");
+    SKOSAutocompleter skosAutocompleter = new SKOSAutocompleter(
+        Version.LUCENE_40, skosFile, "en");
     
     Scanner sc = new Scanner(System.in);
     int numSug = 5;
@@ -25,11 +24,10 @@ public class Main {
       
       String word = sc.nextLine();
       
-      String[] suggestSimilar = skosSuggester.suggestSimilar(word, 1);
+      String[] suggestSimilar = skosAutocompleter.suggestSimilar(word, numSug);
       
       if (suggestSimilar != null && suggestSimilar.length > 0) {
-        String[] suggestSimilar2 = skosAutocompleter.suggestSimilar(suggestSimilar[0], numSug);
-        for (String sug : suggestSimilar2) {
+        for (String sug : suggestSimilar) {
           System.out.println(sug);
         }
       }
